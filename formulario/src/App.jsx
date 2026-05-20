cbf07e42071ad8b6038a75c64e5dba5d3853de4f
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-05-19 23:20:26 -0300

    ....

--- formulario/src/App.jsx
+++ formulario/src/App.jsx
@@ -1,10 +1,17 @@
 import { Braces } from 'lucide-react';
+
 function App() {
   
 
   return (
     <>
-    <h1 style={{textAlign: 'center'}}>Form Learn | <Braces /></h1>
+   
+      <h1 style={{textAlign: 'center'}}>Form Learn | <Braces size={33} style={{marginBottom: '-7px'}} /></h1>
+
+
+    <form>
+    
+    </form>
 
     
       
